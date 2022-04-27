b1aaaa6d687fa91a1d695d6d8e54c6eac5c396df
oldtimer1995 <oldtimer1995@gmail.com>
2022-04-27 19:13:29 +0200

    Ez egy teszt.

--- src/tic/tac/tictactoe.java
+++ src/tic/tac/tictactoe.java
@@ -18,7 +18,7 @@ import javax.swing.JOptionPane;
  */
 public class tictactoe extends javax.swing.JFrame {
 
-    //comment, amit én tettem bele
+    //ez egy fa
     
     private String startGame = "X";
     private int xCount = 0;
