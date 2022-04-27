de38b1871026dd2d4ff2f85a036844f318a6a518
oldtimer1995 <oldtimer1995@gmail.com>
2022-04-27 19:29:26 +0200

    Changes.

--- src/tic/tac/tictactoe.java
+++ src/tic/tac/tictactoe.java
@@ -18,7 +18,7 @@ import javax.swing.JOptionPane;
  */
 public class tictactoe extends javax.swing.JFrame {
 
-    //ez egy fa
+    //alma
     
     private String startGame = "X";
     private int xCount = 0;
